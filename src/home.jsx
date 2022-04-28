2662f5df13470318bed88131c953b9ec94260a42
shridharrr <98966222+shridharrr@users.noreply.github.com>
2022-04-28 13:11:19 +0530

    develop

---
+++ src/home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+function home(){
+    return(
+
+
+        
+    <div className="homepage">
+    <div className="container">
+        <div class="row1">
+            
+            <div className="col-1">
+             <img src="D:\shrii\shrii.design\home\Frame.png" alt=""/>
+             <div class="col-2">
+                <button className="btn"><h2>DAily</h2></button> <br/>
+                <button className="btn"><h2>Ware house</h2></button>
+            </div>
+            
+            </div>
+            <div className="row">
+                <h2>hello</h2>
+            </div>
+
+            
+        </div>
+    </div>
+</div>
+
+
+    );
+    
+}
